364f94f58cc95a8ff0d533739ea9a495125f13f1
Christian <christianmcfa@gmail.com>
2018-03-03 20:57:07 -0500

    set up and tested datagram sockets

--- A231/Receiver/Receiver.java
+++ A231/Receiver/Receiver.java
@@ -1,3 +1,48 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+
+public class Receiver {
+
+	public static void main (String args[]) {
+
+		try {
+
+			//make GUI
+			//get initial values
+
+			//establish connection
+			DatagramSocket dataSocket = new DatagramSocket(4444);//port, dataPort); //send data through
+			DatagramSocket ackSocket = new DatagramSocket(); //receive acks through
+
+			for (int i = 0; i < 2; i++){
+				//receive packets
+				byte[] buf = new byte[1024];  
+	    		DatagramPacket dp = new DatagramPacket(buf, 1024);  
+				dataSocket.receive(dp);
+				String str = new String(dp.getData(), 0, dp.getLength());  
+	    		System.out.println(str);
+
+	    		// dataSocket.receive(dp);
+	    		// String str1 = new String(dp.getData(), 0, dp.getLength());  
+	    		// System.out.println(str1);
+
+
+	    		//send ack packet
+	    		String testack = "ack unloaded";
+				InetAddress ip = InetAddress.getLocalHost();
+				DatagramPacket da = new DatagramPacket(testack.getBytes(), testack.length(), ip, 4445);
+				ackSocket.send(da);
+			}
+
+
+
+			dataSocket.close();
+			ackSocket.close();
+
+		} catch(Exception e) {
+
+			System.err.println("Error in Receiver main: " + e);
+		}
+	}
+}
--- A231/Sender/Sender.java
+++ A231/Sender/Sender.java
@@ -2,3 +2,53 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+public class Sender {
+
+	public static void main(String[] args) {
+
+		try {
+
+			//<host address of the receiver> 
+			// int port = Integer.parseInt(args[0]);
+			// //<UDP port number used by the receiver to receive data from the sender>
+			// int dataPort = Integer.parseInt(args[1]);
+			// //<UDP port number used by the sender to receive ACKs from the receiver>
+			// int ackPort = Integer.parseInt(args[2]);
+			// //<name of the file to be transferred>
+			// String filename = args[3];
+			// //<timeout> integer number for timeout (in microseconds)	
+			// int timeout = Integer.parseInt(args[4]);
+
+			//create datagramSocket
+			DatagramSocket dataSocket = new DatagramSocket();
+			//port, dataPort); //send data through
+			DatagramSocket ackSocket = new DatagramSocket(4445); //receive acks through
+
+			for (int i = 0; i < 2; i++){
+
+				String test = "hot dp received";
+				InetAddress ip = InetAddress.getLocalHost();  
+				//create packets
+				DatagramPacket dp = new DatagramPacket(test.getBytes(), test.length(), ip, 4444);
+				dataSocket.send(dp);
+		
+
+
+				//receive ack
+				byte[] buf = new byte[1024];
+				DatagramPacket da = new DatagramPacket(buf, 1024);
+				ackSocket.receive(da);
+				String strack = new String(da.getData(), 0, da.getLength());  
+				System.out.println(strack);
+			}
+
+
+			dataSocket.close();
+			ackSocket.close();
+
+		} catch(Exception e) {
+
+			System.err.println("Error in Sender main: " + e);
+		}
+	}
+}
